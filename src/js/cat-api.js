8b34438966c51b9483b922625a3197adfb5d705d
Alexandr Liashenko <134228068+AlexLiashenko19@users.noreply.github.com>
2023-08-02 20:24:01 +0300

    Update cat-api.js

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,4 +1,3 @@
-import { Notify } from "notiflix";
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreed() {
@@ -11,12 +10,16 @@ export function fetchBreed() {
 export function fetchCatByBreed(breedId) {
   return fetch(
     `${BASE_URL}/images/search?breed_ids=${breedId}`
-  ).then(res => res.json());
+  ).then(res => {
+    if (!res.ok) throw new Error(res.status);
+    return res.json()
+  }); 
 };
 
 export function catsId(id) {
-  return fetch(`${BASE_URL}/images/${id}`).then(res =>
-    res.json()
-    );
+  return fetch(`${BASE_URL}/images/${id}`).then(res => {
+    if (!res.ok) throw new Error(res.status);
+    return res.json()
+  }); 
 };
 
